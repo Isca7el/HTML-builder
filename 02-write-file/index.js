cea0b1921311f7d7f09caa373e2d3cd24784cd89
coldfear457@gmail.com <coldfear457@gmail.com>
2021-11-01 13:47:53 +0600

    feat: solution write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+const { stdin, exit } = process;
+
+console.log('Write something, that to write in file text.txt: \b');
+
+let writeableStream;
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: writeableStream
+});
+
+rl.on('line', line => {
+  if (line === 'exit') {
+    exit();
+  }
+  if (!writeableStream) writeableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+  writeableStream.write(`${line} \n`);
+});
+
+process.on('exit', () => console.log('\bSee you later'));
+process.on('SIGINT', () => exit());
