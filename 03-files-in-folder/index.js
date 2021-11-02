caca7fd175b48f1fd08b52ec159e55cb55d2178a
coldfear457@gmail.com <coldfear457@gmail.com>
2021-11-02 15:58:20 +0600

    feat: add solution files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folder, (err, files) => {
+  files.forEach(file => {
+    const items = path.join(__dirname, 'secret-folder', file);
+    fs.stat(items, (err, it) => {
+      if (it.isFile()) {
+        const name = path.parse(items).name;
+        const expansion = path.parse(items).ext.slice(1);
+        const size =(it.size / 1024).toFixed(3);
+        console.log(`${name} - ${expansion} - ${size}kb`);
+      }
+    });
+  });
+});
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const folder = path.join(__dirname, 'files');
+const newFolder = pat.join(__dirname, 'files-copy');
+
+
 
