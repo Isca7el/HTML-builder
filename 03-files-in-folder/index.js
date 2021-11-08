91aa6ad1c6f2061be626a8df20d176be8831aeef
coldfear457@gmail.com <coldfear457@gmail.com>
2021-11-08 20:53:32 +0600

    fix: filex-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,16 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const folder = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folder, { withFileTypes: true }, (err, files) => {
-  files.forEach(file => {
-    const items = path.join(__dirname, 'secret-folder', file);
-    fs.stat(items, (err, it) => {
-      if (it.isFile()) {
-        const name = path.parse(items).name;
-        const expansion = path.parse(items).ext.slice(1);
-        const size =(it.size / 1024).toFixed(3);
-        console.log(`${name} - ${expansion} - ${size}kb`);
-      }
+fs.readdir(folder, (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    files.forEach(file => {
+      const items = path.join(__dirname, 'secret-folder', file);
+      fs.stat(items, (err, it) => {
+        if (it.isFile()) {
+          const name = path.parse(items).name;
+          const expansion = path.parse(items).ext.slice(1);
+          const size = (it.size / 1024).toFixed(3);
+          console.log(`${name} - ${expansion} - ${size}kb`);
+        }
+      });
     });
-  });
+  }
 });
